30e1ebb66a51fc777758da69af5290fe4f887f97
T-Bone04 <tashen.jason@gmail.com>
2023-04-20 13:04:25 -0700

    Simple shell working, mandatory tasks

--- main.c
+++ main.c
@@ -4,30 +4,75 @@
 int main(int ac, char **argv)
 {
 	char *prompt = "shell$ "; /* Printing a prompt */
-	char *lineptr; /* Reading and printing what the end user types out */
+	char *lineptr = NULL; /* Reading and printing what the end user types out */
+	char *lineptr_copy = NULL; /* strtok breaks down da string, copy of to use */
 	size_t n = 0;
 	ssize_t nchars_read;
+	const char *delim = " \n";
+	int num_tokens = 0;
+	char *token;
+	int i;
 
 	/* declearing void variables */
-	(void)ac; (void)argv;
+	(void)ac;
 
-	/* create an infinte loop */
+	/* create an infinte loop and shell prompt */
 	while (1)
 	{
 		printf("%s", prompt);
 		nchars_read = getline(&lineptr, &n, stdin);
-		
+
 		/* check if the function getline failed or reached EOF or use CTRL + D */
 		if (nchars_read == -1)
 		{
 			printf("Exiting shell.....\n");
 			return (-1);
 		}
-		
-		printf("%s\n", lineptr);
-   		free(lineptr); /* free up allocated memory */
+
+		/* allocate space for a copy of the lineptr */
+		lineptr_copy = malloc(sizeof(char) * nchars_read);
+		if (lineptr_copy == NULL)
+		{
+			perror("tsh: memory allocation error");
+			return (-1);
+		}
+
+		/* copy lineptr to lineptr_copy */
+		strcpy(lineptr_copy, lineptr);
+
+		/******** split the string (lineptr) into an array of words ******/
+		/* calculate the total number of tokens */
+		token = strtok(lineptr, delim);
+
+		/* determine how many tokens are there*/
+		while (token != NULL) /* check if it is not null with  help of the while */
+		{
+			num_tokens++; /* If it is not null we add one to our counter num_tokens */
+			token = strtok(NULL, delim); /* call strtok, can get the next token */
+		}
+		num_tokens++;
+
+		/* Allocate space to hold the array of strings */
+		argv = malloc(sizeof(char *) * num_tokens);
+
+		/* Store each token in the argv array */
+		token = strtok(lineptr_copy, delim);
+
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[i], token);
+
+			token = strtok(NULL, delim);
+		}
+		argv[i] = NULL;
+
+		/* execute the command */
+		execmd(argv);
 	}
+		/* printf("%s\n", lineptr); */
+		free(lineptr); /* free up allocated memory */
+		free(lineptr);
 
 	return (0);
 }
-
--- shell.h
+++ shell.h
@@ -1,3 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
+/* int execve(const char *pathname, char *const argv[], char *const envp[]); */
+void execmd(char **argv);
