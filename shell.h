8f734204fb3c932ec434ccfdedb940c0ed7ce914
PertuniaZulu <pertuniazulu0@gmail.com>
2023-04-22 09:15:06 +0200

    Edited shell.h

--- shell.h
+++ shell.h
@@ -1,3 +1,6 @@
+#ifdef _SHELL_H_
+#define _SHELL_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,3 +8,8 @@
 
 /* int execve(const char *pathname, char *const argv[], char *const envp[]); */
 void execmd(char **argv);
+/*ge_tline.c*/
+void assign_line(char **buf_str, size_t *n, char *buff, size_t i);
+ssize_t _getline(char **buff_str, size_t *n, FILE *stream);
+
+#endif
