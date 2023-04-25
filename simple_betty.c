df46cd00aae89d9c963027eaf602e0f5bf510e93
PertuniaZulu <pertuniazulu0@gmail.com>
2023-04-25 21:06:49 +0200

    added simple_betty.c

---
+++ simple_betty.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point.
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int num0;
+	int num1;
+
+	printf("First number: ");
+	scanf("%d", &num0);
+	printf("Second number: ");
+	scanf("%d", &num1);
+
+	printf("Results: %d", num0 + num1);
+	return (0);
+}
+
