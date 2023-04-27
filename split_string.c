9d6c784d8429dcffe8d3ef5e2647617f51331c85
T-Bone04 <tashen.jason@gmail.com>
2023-04-27 16:05:46 -0700

    split_string.c

--- split_string.c
+++ split_string.c
@@ -3,14 +3,14 @@
 #include "shell.h"
 
 void split_string(char **argv, char *lineptr, const char *delim) {
-    // Allocate memory for a copy of lineptr
+    /* Allocate memory for a copy of lineptr */
     char *lineptr_copy = safe_malloc(sizeof(char) * strlen(lineptr) + 1);
     strcpy(lineptr_copy, lineptr);
 
-    // Count the number of tokens in lineptr
+    /* Count the number of tokens in lineptr */
     int num_tokens = count_tokens(lineptr, delim);
 
-    // Split lineptr into an array of tokens
+    /* Split lineptr into an array of tokens */
     char *token = strtok(lineptr_copy, delim);
     for (int i = 0; token != NULL; i++) {
         argv[i] = safe_malloc(sizeof(char) * strlen(token) + 1);
@@ -19,7 +19,7 @@ void split_string(char **argv, char *lineptr, const char *delim) {
     }
     argv[num_tokens] = NULL;
 
-    // Free the memory allocated for lineptr_copy
+    /* Free the memory alloated for lineptr_copy */
     free(lineptr_copy);
 }
 
